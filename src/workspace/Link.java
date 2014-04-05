84f0a94ad265eb0ea83119218e759e0d2ff4d4cf
MyCodeCorner <mark@mycodecorner.com>
2014-04-05 15:54:24 -0400

    Second commit
    Bug fixes

--- src/workspace/Link.java
+++ src/workspace/Link.java
@@ -2,6 +2,9 @@ package workspace;
 
 import javax.swing.JOptionPane;
 
+/**
+ * @author Mark Robinson
+ */
 public class Link {
 	public static void main(String[] args) {
 		String[][] carrierData = { { "Verizon", "@vtext.com" },
@@ -14,10 +17,10 @@ public class Link {
 		String carrierSMSAddr = "";
 
 		String selectedCarrier = (String) JOptionPane.showInputDialog(null,
-				"Which carrier would you like to use?", "Select Carrier", -1,
-				null, carriers, "Verizon");
+				"What is the phone's carrier?", "Select Carrier", -1, null,
+				carriers, "Verizon");
 
-		if (!(selectedCarrier == null) && !selectedCarrier.isEmpty()) {
+		if (selectedCarrier != null && !selectedCarrier.isEmpty()) {
 			for (String[] carrier : carrierData) {
 				if (carrier[0].equals(selectedCarrier)) {
 					carrierSMSAddr = carrier[1];
@@ -26,20 +29,23 @@ public class Link {
 		}
 
 		if (!carrierSMSAddr.isEmpty()) {
-			String sendAddr = JOptionPane
-					.showInputDialog(null,
-							"Input the phone number that you'd like to send your text to.");
+			String sendAddr = JOptionPane.showInputDialog(null,
+					"Input the phone's phone number.");
 
-			if ((!sendAddr.isEmpty()) && (sendAddr.length() == 10)) {
+			if (sendAddr != null && !sendAddr.isEmpty()
+					&& sendAddr.length() >= 10) {
 				String message = JOptionPane
-						.showInputDialog(null,
-								"Input the text message that you'd like to send to the phone.");
+						.showInputDialog(
+								null,
+								String.format(
+										"Input the text that you'd like to send to %s on %s.",
+										sendAddr, selectedCarrier));
+				if (message != null && !message.isEmpty()) {
+					carrierSMSAddr = sendAddr.concat(carrierSMSAddr);
 
-				carrierSMSAddr = sendAddr.concat(carrierSMSAddr);
-
-				SendMessage sendHelper = new SendMessage();
-
-				sendHelper.Send(message, carrierSMSAddr);
+					SendMessage sendHelper = new SendMessage();
+					sendHelper.Send(message, carrierSMSAddr);
+				}
 			}
 		}
 	}
--- src/workspace/SendMessage.java
+++ src/workspace/SendMessage.java
@@ -3,59 +3,59 @@ package workspace;
 import java.util.Properties;
 
 import javax.mail.Message;
+import javax.mail.MessagingException;
 import javax.mail.Session;
 import javax.mail.Transport;
+import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import javax.swing.JOptionPane;
 
-public class SendMessage
-{
-  public void Send(String mess, String sendTo)
-  {
-    String host = "smtp.gmail.com";
-    String from = "codecornerapps@gmail.com";
-    String pass = "password";
+/**
+ * @author Mark Robinson
+ */
+public class SendMessage {
+	public void Send(String mess, String sendTo) {
+		String host = "smtp.gmail.com";
+		String from = "codecornerapps@gmail.com";
+		String pass = "password";
 
-    Properties props = System.getProperties();
-    props.put("mail.smtp.starttls.enable", "true");
-    props.put("mail.smtp.host", host);
-    props.put("mail.smtp.user", from);
-    props.put("mail.smtp.password", pass);
-    props.put("mail.smtp.port", "587");
-    props.put("mail.smtp.auth", "true");
+		Properties props = System.getProperties();
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", host);
+		props.put("mail.smtp.user", from);
+		props.put("mail.smtp.password", pass);
+		props.put("mail.smtp.port", "587");
+		props.put("mail.smtp.auth", "true");
 
-    String[] to = { sendTo };
-    try
-    {
-      Session session = Session.getDefaultInstance(props, null);
-      MimeMessage message = new MimeMessage(session);
-      message.setFrom(new InternetAddress(from));
+		String[] to = { sendTo };
+		Session session = Session.getDefaultInstance(props, null);
+		MimeMessage message = new MimeMessage(session);
+		try {
+			message.setFrom(new InternetAddress(from));
 
-      InternetAddress[] toAddress = new InternetAddress[to.length];
+			InternetAddress[] toAddress = new InternetAddress[to.length];
 
-      for (int i = 0; i < to.length; i++) {
-        toAddress[i] = new InternetAddress(to[i]);
-      }
+			for (int i = 0; i < to.length; i++) {
+				toAddress[i] = new InternetAddress(to[i]);
+			}
 
-      for (int i = 0; i < toAddress.length; i++)
-      {
-        message.addRecipient(Message.RecipientType.TO, toAddress[i]);
-      }
-      message.setText(mess);
-      Transport transport = session.getTransport("smtp");
-      transport.connect(host, from, pass);
-      transport.sendMessage(message, message.getAllRecipients());
-      transport.close();
+			for (int i = 0; i < toAddress.length; i++) {
+				message.addRecipient(Message.RecipientType.TO, toAddress[i]);
+			}
 
-      JOptionPane.showMessageDialog(
-        null, 
-        "Message Sent!", 
-        "Successful", 
-        1);
-    }
-    catch (Exception localException)
-    {
-    }
-  }
+			message.setText(mess);
+			Transport transport = session.getTransport("smtp");
+			transport.connect(host, from, pass);
+			transport.sendMessage(message, message.getAllRecipients());
+			transport.close();
+
+			JOptionPane.showMessageDialog(null, "Message Sent!", "Successful",
+					1);
+		} catch (AddressException e) {
+			e.printStackTrace();
+		} catch (MessagingException e) {
+			e.printStackTrace();
+		}
+	}
 }
